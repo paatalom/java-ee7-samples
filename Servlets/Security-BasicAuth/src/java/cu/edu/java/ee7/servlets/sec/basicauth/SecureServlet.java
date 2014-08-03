214ac791976e07203c08a55e6af5da7bce9bf169
paatalom <paata1@192.168.10.103>
2014-08-03 23:13:31 +0400

    Servlet Event Listeners Example

--- Servlets/Security-BasicAuth/src/java/cu/edu/java/ee7/servlets/sec/basicauth/SecureServlet.java
+++ Servlets/Security-BasicAuth/src/java/cu/edu/java/ee7/servlets/sec/basicauth/SecureServlet.java
@@ -1,16 +1,12 @@
 package cu.edu.java.ee7.servlets.sec.basicauth;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * @author Arun Gupta
- */
 @WebServlet(urlPatterns = {"/SecureServlet"})
 public class SecureServlet extends HttpServlet {
     @Override
