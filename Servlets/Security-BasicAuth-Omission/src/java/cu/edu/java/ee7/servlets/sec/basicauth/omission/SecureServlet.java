c46390232dbb17e9522f1e97ec46d79e1a379b83
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-08-03 23:43:20 +0400

    Servlet Basic Authentication Omission

---
+++ Servlets/Security-BasicAuth-Omission/src/java/cu/edu/java/ee7/servlets/sec/basicauth/omission/SecureServlet.java
@@ -0,0 +1,24 @@
+package cu.edu.java.ee7.servlets.sec.basicauth.omission;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SecureServlet")
+public class SecureServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().print("my GET");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().print("my POST");
+    }
+}
