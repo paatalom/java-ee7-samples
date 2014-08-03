900ef0a240bb4650db0d706ad973eaa3ac2c3ff4
paatalom <paata1@192.168.10.103>
2014-08-03 23:26:13 +0400

    Servlet Event Listeners Example

--- Servlets/Security-BasicAuth/src/java/cu/edu/java/ee7/servlets/sec/basicauth/SecureServlet.java
+++ Servlets/Security-BasicAuth/src/java/cu/edu/java/ee7/servlets/sec/basicauth/SecureServlet.java
@@ -2,13 +2,17 @@ package cu.edu.java.ee7.servlets.sec.basicauth;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = {"/SecureServlet"})
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = {"g1"}))
 public class SecureServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
