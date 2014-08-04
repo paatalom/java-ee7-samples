395fc503b2829d8b95701d72e060247143840534
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-08-05 01:07:07 +0400

    Protocol Handler Example

---
+++ Servlets/Protocol-Handler/src/java/cu/edu/java/ee7/servlets/protocolhandler/MyProtocolHandler.java
@@ -0,0 +1,29 @@
+package cu.edu.java.ee7.servlets.protocolhandler;
+
+import java.io.IOException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpUpgradeHandler;
+import javax.servlet.http.WebConnection;
+
+/**
+ * @author Arun Gupta
+ */
+public class MyProtocolHandler implements HttpUpgradeHandler {
+
+//    public MyProtocolHandler(ServletInputStream in, ServletOutputStream out) {
+//    }
+
+    @Override
+    public void init(WebConnection wc) {
+        try (ServletInputStream input = wc.getInputStream();
+            ServletOutputStream output = wc.getOutputStream();) {
+        } catch (IOException ex) {
+        }
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
