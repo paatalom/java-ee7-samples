e0179e802f8cc9400e872c25589be9b149a9b6a8
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-06 19:14:58 +0400

    HTTP GET Method Example

---
+++ jsf/HttpGET/src/java/cu/edu/java/ee7/jsf/http/get/User.java
@@ -0,0 +1,33 @@
+package cu.edu.java.ee7.jsf.http.get;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ComponentSystemEvent;
+import javax.inject.Named;
+
+@Named
+@ApplicationScoped
+public class User {
+
+    private String name;
+
+    public String getName() {
+        System.out.println("getName: " + name);
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Both process and process2 are valid method signatures
+    public void process(ComponentSystemEvent event) throws AbortProcessingException {
+        System.out.println("process called");
+        name = name.toUpperCase();
+    }
+    
+    public void process2() {
+        System.out.println("process2 called");
+        name = name.toUpperCase();
+    }
+}
