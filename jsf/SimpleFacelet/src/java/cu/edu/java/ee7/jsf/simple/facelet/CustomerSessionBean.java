ae4c6eb58d41fcd72d950be230a6f90c7dfd9836
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-06 16:13:06 +0400

    JSF Examples

---
+++ jsf/SimpleFacelet/src/java/cu/edu/java/ee7/jsf/simple/facelet/CustomerSessionBean.java
@@ -0,0 +1,27 @@
+package cu.edu.java.ee7.jsf.simple.facelet;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+
+
+@Named
+@Stateless
+public class CustomerSessionBean {
+    
+    public List<Name> getCustomerNames() {
+        List<Name> names = new ArrayList<>();
+        
+        names.add(new Name("Penny", "TBBT"));
+        names.add(new Name("Sheldon", "TBBT"));
+        names.add(new Name("Amy", "TBBT"));
+        names.add(new Name("Leonard", "TBBT"));
+        names.add(new Name("Bernadette", "TBBT"));
+        names.add(new Name("Raj", "TBBT"));
+        names.add(new Name("Priya", "TBBT"));
+        names.add(new Name("Howard", "TBBT"));
+        
+        return names;
+    }
+}
