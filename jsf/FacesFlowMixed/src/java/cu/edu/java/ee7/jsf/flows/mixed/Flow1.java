323b4813b6924eea4c6bff2039201c66b998ec7d
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-08 15:09:11 +0400

    JSF Flow Mixed Example

---
+++ jsf/FacesFlowMixed/src/java/cu/edu/java/ee7/jsf/flows/mixed/Flow1.java
@@ -0,0 +1,31 @@
+package cu.edu.java.ee7.jsf.flows.mixed;
+
+import javax.enterprise.inject.Produces;
+import javax.faces.flow.Flow;
+import javax.faces.flow.builder.FlowBuilder;
+import javax.faces.flow.builder.FlowBuilderParameter;
+import javax.faces.flow.builder.FlowDefinition;
+
+public class Flow1 {
+
+    @Produces @FlowDefinition
+    public Flow defineFlow(@FlowBuilderParameter FlowBuilder flowBuilder) {
+        String flowId = "flow1";
+        flowBuilder.id("", flowId);
+        flowBuilder.viewNode(flowId, "/" + flowId + "/" + flowId + ".xhtml").markAsStartNode();
+        
+        flowBuilder.returnNode("taskFlowReturn1").
+                fromOutcome("#{flow1Bean.returnValue}");
+        flowBuilder.returnNode("goHome").
+                fromOutcome("#{flow1Bean.homeValue}");
+        
+        flowBuilder.inboundParameter("param1FromFlow2", "#{flowScope.param1Value}");
+        flowBuilder.inboundParameter("param2FromFlow2", "#{flowScope.param2Value}");
+        
+        flowBuilder.flowCallNode("call2").flowReference("", "flow2").
+                outboundParameter("param1FromFlow1", "param1 flow1 value").
+                outboundParameter("param2FromFlow1", "param2 flow1 value");
+
+        return flowBuilder.getFlow();
+    }
+}
---
+++ jsf/FacesFlowMixed/src/java/cu/edu/java/ee7/jsf/flows/mixed/Flow1Bean.java
@@ -0,0 +1,22 @@
+package cu.edu.java.ee7.jsf.flows.mixed;
+
+import java.io.Serializable;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+
+@Named
+@FlowScoped("flow1")
+public class Flow1Bean implements Serializable {
+
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    public String getReturnValue() {
+        return "/return";
+    }
+
+    public String getHomeValue() {
+        return "/index";
+    }    
+}
