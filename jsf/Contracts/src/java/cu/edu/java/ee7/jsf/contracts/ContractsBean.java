f9cc5b450e1f8c7ebc8bc12d2167c07f18a0ba4b
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-08 18:37:01 +0400

    JSF Contracts Example

---
+++ jsf/Contracts/src/java/cu/edu/java/ee7/jsf/contracts/ContractsBean.java
@@ -0,0 +1,23 @@
+package cu.edu.java.ee7.jsf.contracts;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * @author Arun Gupta
+ */
+@Named
+@SessionScoped
+public class ContractsBean implements Serializable {
+    
+    String contract = "red";
+
+    public String getContract() {
+        return contract;
+    }
+
+    public void setContract(String contract) {
+        this.contract = contract;
+    }
+}
