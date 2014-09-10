264c6c531a6b06716083cce7593a1f65f853e3d7
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-11 00:45:30 +0400

    jsf radio buttons example

---
+++ jsf/RadioButtons/src/java/cu/edu/java/ee7/jsf/radio/buttons/Movie.java
@@ -0,0 +1,55 @@
+package cu.edu.java.ee7.jsf.radio.buttons;
+
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Arun Gupta
+ */
+public class Movie {
+    @Id
+    @NotNull
+    private Integer id;
+    
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String name;
+    
+    @NotNull
+    @Size(min = 1, max = 200)
+    private String actors;
+
+    public Movie() {
+    }
+
+    public Movie(Integer id, String name, String actors) {
+        this.id = id;
+        this.name = name;
+        this.actors = actors;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }    
+}
---
+++ jsf/RadioButtons/src/java/cu/edu/java/ee7/jsf/radio/buttons/MovieBean.java
@@ -0,0 +1,37 @@
+package cu.edu.java.ee7.jsf.radio.buttons;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * @author Arun Gupta
+ */
+@Named
+@SessionScoped
+public class MovieBean implements Serializable {
+
+    private static final List<Movie> list = Arrays.asList(
+            new Movie(1, "The Matrix", "Keanu Reeves"),
+            new Movie(2, "The Lord of the Rings", "Elijah Wood"),
+            new Movie(3, "The Inception", "Leonardo Dicaprio"));
+    int selected;
+
+    public int getSelected() {
+        return selected;
+    }
+
+    public void setSelected(int selected) {
+        this.selected = selected;
+    }
+
+    public List<Movie> getAll() {
+        return list;
+    }
+
+    public Movie getSelectedMovie() {
+        return list.get(selected-1);
+    }
+}
