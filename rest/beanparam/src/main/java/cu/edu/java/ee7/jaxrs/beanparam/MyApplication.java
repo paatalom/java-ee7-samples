3916f17c2653a7aae7d289b433f31cc5ba036d4a
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-21 18:09:50 +0400

    Bean Param Example

---
+++ rest/beanparam/src/main/java/cu/edu/java/ee7/jaxrs/beanparam/MyApplication.java
@@ -0,0 +1,9 @@
+package cu.edu.java.ee7.jaxrs.beanparam;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webresources")
+public class MyApplication extends Application {
+
+}
