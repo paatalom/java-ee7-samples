dee1837e3bd931f1c2b91598f7eb5dd702db6143
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-22 01:54:49 +0400

    Restful WS Asynchronous Example

---
+++ rest/invocation-async/src/main/java/cu/edu/java/ee7/jaxrs/invocation/async/MyResource.java
@@ -0,0 +1,27 @@
+package cu.edu.java.ee7.jaxrs.invocation.async;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("resource")
+public class MyResource {
+
+    @GET
+    @Produces("text/plain")
+    public String get() {
+        System.out.println("get");
+        return "foobar";
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public String post(@FormParam("name") String name, @FormParam("age") int age) {
+        System.out.println("post");
+        return name + String.valueOf(age);
+    }
+}
