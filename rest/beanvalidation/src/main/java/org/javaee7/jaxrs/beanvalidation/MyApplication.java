64605e42638d45bd3b330d69059d1a14689a6153
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-21 18:17:55 +0400

    Bean Validation Example

---
+++ rest/beanvalidation/src/main/java/org/javaee7/jaxrs/beanvalidation/MyApplication.java
@@ -0,0 +1,9 @@
+package org.javaee7.jaxrs.beanvalidation;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webresources")
+public class MyApplication extends Application {
+
+}
---
+++ rest/beanvalidation/src/main/java/org/javaee7/jaxrs/beanvalidation/MyResource.java
@@ -0,0 +1,27 @@
+package org.javaee7.jaxrs.beanvalidation;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/endpoint")
+public class MyResource {
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String post(@Size(min = 3) String payload) {
+        return payload;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void post2(@NotNull @FormParam("name")String name, @Min(1) @Max(10)@FormParam("age")int age) {
+    }
+}
