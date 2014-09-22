6f7cc9bbfdef5b6cec6393b996df2d8ef8b8f41f
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-09-22 18:00:48 +0400

    JaxRS Security Example

---
+++ rest/jaxrs-security-declarative/src/main/java/cu/edu/java/ee7/jaxrs/security/declarative/MyResource.java
@@ -0,0 +1,43 @@
+package cu.edu.java.ee7.jaxrs.security.declarative;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("myresource")
+@RequestScoped
+public class MyResource {
+    @GET
+    public String get() {
+        return "get";
+    }
+    
+    @GET
+    @Path("{id}")
+    public String getPerson(@PathParam("id")int id) {
+        return "get" + id;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public String addToList(@FormParam("name") String name) {
+        return "post " + name;
+    }
+
+    @PUT
+    public void putToList() {
+        System.out.println("put invoked");
+    }
+
+    @DELETE
+    public void delete() {
+        System.out.println("delete invoked");
+    }
+}
