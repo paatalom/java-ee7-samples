6d59fdcf5e13bb1a67033ca7a5e7fc96856a4db9
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-10-12 01:55:57 +0400

    Web Socket Client Config

---
+++ web-sockets/WebSocketClientConfig/src/java/cu/edu/java/ee7/websocket/client/configuration/MyConfigurator.java
@@ -0,0 +1,30 @@
+package cu.edu.java.ee7.websocket.client.configuration;
+
+import java.util.List;
+import java.util.Map;
+import javax.websocket.ClientEndpointConfig;
+import javax.websocket.HandshakeResponse;
+
+ public class MyConfigurator extends ClientEndpointConfig.Configurator {
+
+    @Override
+    public void beforeRequest(Map<String, List<String>> headers) {
+        System.out.println("beforeRequest:");
+        for (String h : headers.keySet()) {
+            for (String k : headers.get(h)) {
+                System.out.println("Header: " + h + ", " + k);
+            }
+        }
+
+    }
+
+    @Override
+    public void afterResponse(HandshakeResponse response) {
+        System.out.println("afterResponse:");
+        for (String h : response.getHeaders().keySet()) {
+            for (String k : response.getHeaders().get(h)) {
+                System.out.println("Header: " + h + ", " + k);
+            }
+        }
+    }
+}
