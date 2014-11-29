361cde7699e70b32016ccc441d098858572b4398
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-11-29 20:07:59 +0400

    JPA Native SQL

---
+++ jpa/JPANativeSQL/src/java/cu/edu/java/ee7/jpa/nativesql/Employee.java
@@ -0,0 +1,40 @@
+package cu.edu.java.ee7.jpa.nativesql;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EMPLOYEE_NATIVE_SQL")
+public class Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    private int id;
+    
+    @Column(length=50)
+    private String name;
+    
+    public Employee() { }
+    
+    public Employee(String name) {
+        this.name = name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
