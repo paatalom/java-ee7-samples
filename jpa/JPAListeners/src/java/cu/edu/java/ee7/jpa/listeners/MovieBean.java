23ae0b2e29da236d0d4c602f6146b82312615452
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-11-30 15:09:22 +0400

    JPA Listener Examples

---
+++ jpa/JPAListeners/src/java/cu/edu/java/ee7/jpa/listeners/MovieBean.java
@@ -0,0 +1,39 @@
+package cu.edu.java.ee7.jpa.listeners;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class MovieBean {
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Movie> listMovies() {
+        return em.createNamedQuery("Movie.findAll", Movie.class).getResultList();
+    }
+
+    public void createMovie() {
+        Movie m = new Movie(5, "Mission Impossible", "Tom Cruise, Jeremy Renner");
+        em.persist(m);
+        em.flush();
+    }
+
+    public void updateMovie() {
+        Movie m = em.createNamedQuery("Movie.findByName", Movie.class)
+                    .setParameter("name", "Inception")
+                    .getSingleResult();
+        m.setName("Inception2");
+        em.merge(m);
+        em.flush();
+    }
+
+    public void deleteMovie() {
+        Movie m = em.createNamedQuery("Movie.findByName", Movie.class)
+                    .setParameter("name", "Inception2")
+                    .getSingleResult();
+        em.remove(m);
+        em.flush();
+    }
+}
---
+++ jpa/JPAListeners/src/java/cu/edu/java/ee7/jpa/listeners/TestServletCreate.java
@@ -0,0 +1,84 @@
+package cu.edu.java.ee7.jpa.listeners;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "TestServletCreate", urlPatterns = {"/TestServletCreate"})
+public class TestServletCreate extends HttpServlet {
+    
+    @EJB MovieBean bean;
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet TestServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet TestServlet at " + request.getContextPath() + "</h1>");
+
+            bean.createMovie();
+            out.println("Movie Created Successfully <br>");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
