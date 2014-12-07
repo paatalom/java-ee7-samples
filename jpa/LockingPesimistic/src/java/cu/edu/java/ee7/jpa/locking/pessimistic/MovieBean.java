b43b7a826dee11bea9ef32e78e9e6f4988573a47
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-12-07 16:47:15 +0400

    Locking Pessimistic Example

---
+++ jpa/LockingPesimistic/src/java/cu/edu/java/ee7/jpa/locking/pessimistic/MovieBean.java
@@ -0,0 +1,33 @@
+package cu.edu.java.ee7.jpa.locking.pessimistic;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class MovieBean {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public List<Movie> listMovies() {
+        return em.createNamedQuery("Movie.findAll", Movie.class).getResultList();
+    }
+
+    public void updateMovie() {
+        Movie m = em.find(Movie.class, 3, LockModeType.PESSIMISTIC_WRITE);
+        em.lock(m, LockModeType.PESSIMISTIC_WRITE);
+        m.setName("INCEPTION");
+        em.merge(m);
+        em.flush();
+    }
+    
+    public void deleteMovie() {
+        Movie m = em.find(Movie.class, 1, LockModeType.PESSIMISTIC_WRITE);
+        em.remove(m);
+        em.flush();
+    }
+    
+}
