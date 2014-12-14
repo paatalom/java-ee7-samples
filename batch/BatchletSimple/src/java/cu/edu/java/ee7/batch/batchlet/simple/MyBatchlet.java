96f52343e1e275f3c047c6b15a782fead9264a82
paatalom <paata1@Paatas-MacBook-Pro.local>
2014-12-14 14:25:08 +0400

    Batchlet Simple Example

---
+++ batch/BatchletSimple/src/java/cu/edu/java/ee7/batch/batchlet/simple/MyBatchlet.java
@@ -0,0 +1,18 @@
+package cu.edu.java.ee7.batch.batchlet.simple;
+
+import javax.batch.api.AbstractBatchlet;
+import javax.batch.runtime.BatchStatus;
+import javax.inject.Named;
+
+/**
+ * @author Arun Gupta
+ */
+@Named
+public class MyBatchlet extends AbstractBatchlet {
+    @Override
+    public String process() {
+        System.out.println("Running inside a batchlet");
+
+        return BatchStatus.COMPLETED.toString();
+    }
+}
---
+++ batch/BatchletSimple/web/index.jsp
@@ -0,0 +1,12 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Batch : Simple Batchlet</title>
+    </head>
+    <body>
+        <h1>Batch : Simple Batchlet</h1>
+        Invoke the <a href="${pageContext.request.contextPath}/TestServlet"/>Batchlet Servlet</a>.
+    </body>
+</html>
